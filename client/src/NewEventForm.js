281197ef34b1e449e3f21429f55c33176b4fa36e
Michelle <clairsin@gmail.com>
2022-07-12 19:49:18 -0400

    updated seed data

--- client/src/NewEventForm.js
+++ client/src/NewEventForm.js
@@ -10,17 +10,27 @@ function NewEventForm() {
   })
 
   function handleChange(e) {
+    console.log(e.target)
     const {name, value} = e.target
     console.log(name, value)
     setFormData((formData) => ({...formData,
       [name]: value
     }));
+    console.log(formData)
   }
 
   function handleSubmit(e) {
-
+    e.preventDefault()
+    fetch("/create", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json"
+      },
+      body: JSON.stringify(formData),
+    })
+      .then(res => res.json())
+      .then(data => console.log(data))
   }
-
 
   return (
     <div className="App">
@@ -34,30 +44,30 @@ function NewEventForm() {
         <label
           for='game'
         > Game: </label>
-        <select id="game" name="game">
+        <select id="game" name="game" onChange={handleChange} value={formData.game}>
           <option value="" disable selected>Select a game</option>
-          <option value={formData.game} onchange={handleChange}>Basketball-Full Court</option>
-          <option value={formData.game} onchange={handleChange}>Basketball-Half Court</option>
-          <option value={formData.game} onchange={handleChange}>Baseball</option>
-          <option value={formData.game} onchange={handleChange}>Pickleball</option>
-          <option value={formData.game} onchange={handleChange}>Handball-Singles</option>
-          <option value={formData.game} onchange={handleChange}>Handball-Doubles</option>
-          <option value={formData.game} onchange={handleChange}>Tennis-Singles</option>
-          <option value={formData.game} onchange={handleChange}>Tennis-Doubles</option>
-          <option value={formData.game} onchange={handleChange}>Poker</option>
-          <option value={formData.game} onchange={handleChange}>Volleyball</option>
+          <option value="Basketball-Full Court">Basketball-Full Court</option>
+          <option value="Basketball-Half Court">Basketball-Half Court</option>
+          <option value="Baseball">Baseball</option>
+          <option value="Pickleball">Pickleball</option>
+          <option value="Handball-Singles">Handball-Singles</option>
+          <option value="Handball-Doubles">Handball-Doubles</option>
+          <option value="Tennis-Singles">Tennis-Singles</option>
+          <option value="Tennis-Doubles">Tennis-Doubles</option>
+          <option value="Poker">Poker</option>
+          <option value="Volleyball">Volleyball</option>
         </select>
       <br></br>
       <br></br>
         <label
           for='venue'
         > Venue: </label>
-        <select id="venue" name="venue">
+        <select id="venue" name="venue" onChange={handleChange} value={formData.venue}>
           <option value="" disable selected>Select a venue</option>
-          <option value={formData.venue} onchange={handleChange}>Park1</option>
-          <option value={formData.venue} onchange={handleChange}>Park2</option>
-          <option value={formData.venue} onchange={handleChange}>Park3</option>
-          <option value={formData.venue} onchange={handleChange}>Park4</option>
+          <option value="Park1">Park1</option>
+          <option value="Park2">Park2</option>
+          <option value="Park3">Park3</option>
+          <option value="Park4">Park4</option>
         </select>
       <br></br>
       <br></br>
@@ -65,7 +75,7 @@ function NewEventForm() {
           <div className="col-md-4">
             <Form.Group controlId="dob">
               <Form.Label> Date: </Form.Label>
-              <Form.Control type="date" name="date" onchange={handleChange} value={formData.date} />
+              <Form.Control type="date" name="date" onChange={handleChange} value={formData.date} />
             </Form.Group>
           </div>
         </div>
@@ -73,79 +83,19 @@ function NewEventForm() {
         <label
           for='time'
         > Time: </label>
-        <select id="time" name="time">
-          {/* <placeholder="select a time"> */}
+        <select id="time" name="time" onChange={handleChange} value={formData.time}>
           <option value="" disable selected>Select a time</option>
-          <option value={formData.time} onchange={handleChange}>12:00PM</option>
-          <option value={formData.time} onchange={handleChange}>3:00PM</option>
-          <option value={formData.time} onchange={handleChange}>6:00PM</option>
-          <option value={formData.time} onchange={handleChange}>9:00PM</option>
+          <option value="12:00PM">12:00PM</option>
+          <option value="03:00PM">3:00PM</option>
+          <option value="06:00PM">6:00PM</option>
+          <option value="09:00PM">9:00PM</option>
         </select>
         <br></br>
         <br></br>
-        <input
-          type="button"
-          name="submit"
-          value="Create"
-        />
+        <button type="button" name="submit">Create</button>
       </form>
       </div>
       </div>
-      {/* <form className="add-toy-form small">
-        <h4>Add New Game</h4>
-        <input
-          type="text"
-          name="name"
-          value={formData.name}
-          onChange={handleChange}
-          placeholder="Event Name"
-          className="input-text"
-        />
-        <br />
-        <input
-          type="text"
-          name="date"
-          value={formData.date}
-          onChange={handleChange}
-          placeholder="Date: YYYY/MM/DD"
-          className="input-text"
-        />
-        <br />
-        <input
-          type="text"
-          name="time"
-          value={formData.time}
-          onChange={handleChange}
-          placeholder="Time: HH:MM AM/PM"
-          className="input-text"
-        />
-        <br />
-        <input
-          type="text"
-          name="venue"
-          value={formData.venue}
-          onChange={handleChange}
-          placeholder="Venue"
-          className="input-text"
-        />
-        <br />
-        <input
-          type="text"
-          name="artist"
-          value={formData.artist}
-          onChange={handleChange}
-          placeholder="Artist"
-          className="input-text"
-        />
-        <br></br>
-        <br />
-        <input
-          type="submit"
-          name="submit"
-          value="Add Event"
-          className="submit"
-        />
-      </form> */}
     </div>
   )
 }
